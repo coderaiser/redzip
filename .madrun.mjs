e1b30de4440b8716edd2fdadd3680e93e61d6553
coderaiser <coderaiser@cloudcmd.io>
2021-01-20 22:30:13 +0200

    feature(redzip) split local and zip vfs into separete directories

--- .madrun.mjs
+++ .madrun.mjs
@@ -1,11 +1,11 @@
 import {run} from 'madrun';
 
 export default {
-    'test': () => `tape 'lib/**/*.spec.js'`,
+    'test': () => `tape 'lib/**/*.spec.js' -f tap`,
     'report': () => 'nyc report --reporter=text-lcov | coveralls',
     'coverage': () => 'nyc npm test',
     'watch:coverage': () => run('watcher', 'npm run coverage'),
-    'watch:test': () => run('watcher', 'npm test'),
+    'watch:test': async () => await run('watcher', `"${await run('test')}"`),
     'watcher': () => 'nodemon -w test -w lib --exec',
     'lint': () => 'putout .',
     'fresh:lint': () => run('lint', '--fresh'),
